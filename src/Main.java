70b45f7f61df79daa212bdd8adfeaab5684a64eb
Christober <christober2907@gmail.com>
2023-01-24 12:02:53 +0530

    array

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         //Array
+        int[] arr = new int[5];
+
+        arr[0] = 23;
+        arr[1] = 33;
+        arr[2] = 534;
+        arr[3] = 35;
+        arr[4] = 56;
+
+        System.out.println(arr[2]);
+
     }
 }
