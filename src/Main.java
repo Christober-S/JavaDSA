244bb2cd034faaee3d68733774b18f271ff8522e
Christober <christober2907@gmail.com>
2023-01-24 12:19:11 +0530

    getting input from user

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //Array
-        int[] arr = new int[5];
+        //Getting input from user
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the size:");
+        int size = scanner.nextInt();
+        int[] arr = new int[size];
+        
+        for (int i = 0; i<arr.length; i++){
+            arr[i] = scanner.nextInt();
+        }
 
-        arr[0] = 23;
-        arr[1] = 33;
-        arr[2] = 534;
-        arr[3] = 35;
-        arr[4] = 56;
-
-        System.out.println(arr[2]);
+        System.out.println("Printing array values: ");
+        for (int i = 0; i<arr.length; i++){
+            System.out.println(arr[i]);
+        }
 
     }
 }
